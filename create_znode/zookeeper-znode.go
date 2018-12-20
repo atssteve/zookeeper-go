040634d5cdfcbefefffa9a926d5bd91008243f0f
atssteve <steve.horan@theatsgroup.com>
2018-12-19 20:41:37 -0500

    Adding comments

--- create_znode/zookeeper-znode.go
+++ create_znode/zookeeper-znode.go
@@ -16,17 +16,22 @@ func main() {
 		panic(err)
 	}
 
-	//
+	// Store an int value for the binary number 11111 (turn everything on)
 	perms, _ := strconv.ParseInt("11111", 2, 64)
+	// Create a World readable/writable ACL
 	acl := zk.WorldACL(int32(perms))
+	// A byte slice of data to store.
 	data := []byte("Testing")
+	// Create znode
 	connection.Create("/tester", data, 0, acl)
 
+	// Print out the znodes
 	children, _, err := connection.Children("/")
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(children)
+	// Print out the ACL we just created.
 	fmt.Println(connection.GetACL("/tester"))
 }
